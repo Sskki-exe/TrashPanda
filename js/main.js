196121ce1ee1acff8f548bafc3d295e0b99e1702
Hannah <htay0008@student.monash.edu>
2022-02-25 21:56:08 +1100

    buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // JS for the home page
+
+// navigation buttons
+function navHome() {
+    window.location("index.html");
+}
+
+function navIdentify() {
+    window.location("id.html");
+}
+
+function navCompost() {
+    window.location("compost.html");
+}
+
+function navSus() {
+    window.location("sus.html");
+}
